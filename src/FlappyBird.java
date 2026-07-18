285398b43275fad4357e71b06e62b053cd50ac15
Israe <israe.yab@gmail.com>
2026-07-18 21:02:53 +0100

    feat: Add the game timer

--- src/FlappyBird.java
+++ src/FlappyBird.java
@@ -4,9 +4,7 @@ import java.util.ArrayList;
 import java.util.Random;
 import javax.swing.*;
 
-
-
-public class FlappyBird extends JPanel {
+public class FlappyBird extends JPanel implements ActionListener {
     int boardWidth = 360;
     int boardHeight = 640;
 
@@ -15,17 +13,46 @@ public class FlappyBird extends JPanel {
     Image topPipeImg;
     Image bottomPipeImg;
 
+
+    //Bird 
+    int birdX = boardWidth/8;
+    int birdY = boardHeight/2;
+    int birdWidth = 34;
+    int birdHeight = 24;
+
+    class Bird {
+        int x = birdX;
+        int y = birdY;
+        int width = birdWidth;
+        int height = birdHeight;
+        Image img;
+
+        Bird(Image img) {
+            this.img = img;
+        }
+    }
+
+    Bird bird;
+    Timer gameLoop;
+    
+
     FlappyBird() {
         setPreferredSize(new Dimension(boardWidth, boardHeight));
-        //setBackground(Color.BLUE);
+        
 
         backgroundImg = new ImageIcon(getClass().getResource("/flappybirdbg.png")).getImage();
         birdImg = new ImageIcon(getClass().getResource("/bird.png")).getImage();
         topPipeImg = new ImageIcon(getClass().getResource("/toppipe.png")).getImage();
         bottomPipeImg = new ImageIcon(getClass().getResource("/bottompipe.png")).getImage();
+         
+    //bird
+        bird = new Bird(birdImg);
+
+    //game timer
+        gameLoop = new Timer(1000/60, this);
+        gameLoop.start();
+
     }
-
-
 
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
@@ -34,6 +61,15 @@ public class FlappyBird extends JPanel {
     }
 
     public void draw(Graphics g) {
-        g.drawImage(backgroundImg, 0, 0, boardWidth, boardHeight, null);;
+        System.out.println("Draw");
+        g.drawImage(backgroundImg, 0, 0, boardWidth, boardHeight, null);
+        
+        g.drawImage(bird.img, bird.x, bird.y, bird.width, bird.height, null);
     }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        repaint();
+      
+}
 }
